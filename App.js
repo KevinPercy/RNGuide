36192465541bb712c2549b174ebcd7944bfa235d
KevinPercy <kevinpercycl@gmail.com>
2020-06-29 18:47:10 -0500

    Added core components, pass data between components

--- App.js
+++ App.js
@@ -1,43 +1,50 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import {
+  StyleSheet
+  , View
+  , Button
+  , FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = (goalTitle) => {
     // setCourseGoals([...courseGoals, enteredGoal]);
     setCourseGoals(currentGoals => [
       ...currentGoals,
-      { uid: Math.random().toString(), value: enteredGoal }]);
+      { id: Math.random().toString(), value: goalTitle }]);
+    setIsAddMode(false);
   }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    })
+  }
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer} >
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler} //without parenthesis to register every keystroke
-          value={enteredGoal}
-        />
-        <Button
-          title="ADD"
-          onPress={addGoalHandler} />
-      </View>
+      <Button title="Add New Goal" onPress={() => { setIsAddMode(true) }} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
       <FlatList
-        keyExtractor={(item, index) => item.uid}
+        keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text >
-              {itemData.item.value}
-            </Text>
-          </View>
-        )}
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />)}
       />
     </View>
   );
@@ -47,24 +54,7 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50,
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1,
 
-  }
+
 });
 
